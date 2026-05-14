0f8bca1dc9916c097a83dada193da773bec9bb6e
Catarina <kitcatpn@eaportal.org>
2026-05-14 10:48:48 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import mysql2 from "mysql2"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response)=> {
+    response.json({
+        message: "Servidor do ToDo"
+    })
+})
+
+app.post("/create-task", (request, response) => {
+    const {description, status} = request.body
+
+    const insertCommand = "INSERT INTO ToDo_CatarinaPalomares(description, status) VALUES (?, ?)"
+
+    sql.query(insertCommand, [description, status], (error) => {
+        if(error){
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({
+            message: "Tarefa criada com sucesso"
+        })
+    })
+})
+
+app.listen(3000, () => console.log("Servidor ONLINE"))
+
+const sql = mysql2.createPool({
+    host: "benserverplex.ddns.net",
+    user: "alunos",
+    password: "senhaAlunos",
+    database: "todo_03mb"
+})
